dcf447a647a326531f26cefaa28e5e744dff498a
Michał Czyż <michalczyz@gmail.com>
2017-02-27 11:53:04 +0100

    Update factorialize_by_recursion.rb

--- factorialize_by_recursion.rb
+++ factorialize_by_recursion.rb
@@ -1,16 +1,35 @@
-class Factorialize
+# if you write a class treat is as a class 
+class Factorialize < Struct.new(:n)
+  def to_s
+    factorialize(n)
+  end
 
-    def self.factorialize(n)
-        if(n < 0)
-            -1
-        elsif(n == 0)
-            1
-        else n * factorialize(n - 1)
-        end
+  def self.factorialize(n)
+    if(n < 0)
+      -1
+    elsif(n == 0)
+      1
+    else
+      n * factorialize(n - 1)
     end
+  end
 end
 
-p Factorialize.factorialize(1)
+# not sure what is the exercise, still it is not good to execute class in the same file where definition is
+# usage `p Factorialize.new(1)`
 
+# if you want to use it as a module then write it as a module :-)     
 
+module Factorize
+  def self.factorialize(n)
+    if(n < 0)
+      -1
+    elsif(n == 0)
+      1
+    else
+      n * factorialize(n - 1)
+    end
+  end
+end
 
+# usage `p Factorialize.factorialize(1)`
